7fb894786366ce5fc07d0ef7a3c60dc08869dc2b
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-02-14 07:06:37 -0800

    [UPS] added an address implementation

---
+++ lib/Buzz/Extension/UPS/Resource/Address.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Buzz\Extension\UPS\Resource;
+
+class Address
+{
+    private $city;
+    private $stateProvinceCode;
+    private $postalCode;
+    private $countryCode;
+
+    public function __construct($city = null, $stateProvinceCode = null, $postalCode = null, $countryCode = null)
+    {
+        $this->city = $city;
+        $this->stateProvinceCode = $stateProvinceCode;
+        $this->postalCode = $postalCode;
+        $this->countryCode = $countryCode;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    public function getStateProvinceCode()
+    {
+        return $this->stateProvinceCode;
+    }
+
+    public function setStateProvinceCode($stateProvinceCode)
+    {
+        $this->stateProvinceCode = $stateProvinceCode;
+    }
+
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    public function getCountryCode()
+    {
+        return $this->countryCode;
+    }
+
+    public function setCountryCode($countryCode)
+    {
+        $this->countryCode = $countryCode;
+    }
+}
